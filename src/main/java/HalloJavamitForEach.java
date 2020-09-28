40684ecba2acbed6b4dc87cb2cf03ddc3c6cd7c8
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-09-28 13:32:55 +0200

    foreach output done

---
+++ src/main/java/HalloJavamitForEach.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+
+    ArrayList<String> list = new ArrayList<>();
+
+    public void forEachoutput(){
+        for (String s:list) {
+            System.out.println(s);
+        }
+    }
+}
