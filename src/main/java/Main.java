3d35a2d038220cb659f4a920c69aecf8a4c82dab
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-09-28 14:54:47 +0200

    oddevenTester done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,16 @@ public class Main {
     public static void main(String[] args) {
         NumberTester tester = new NumberTester("data");
         tester.setOddevenTester((number) -> (number %2) ==0);
+        tester.setPrimeTester(number ->{
+            if (number <= 1) {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        });
     }
 }
--- src/main/java/NumberTester.java
+++ src/main/java/NumberTester.java
@@ -1,6 +1,31 @@
-public class NumberTester {
-    public NumberTester(String fileName){
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class NumberTester {
+    int numberOfTests;
+
+    public NumberTester(String fileName) {
+        Map<Integer,Integer> numbers = new HashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+            String line = br.readLine();
+            numberOfTests = Integer.parseInt(line);
+            line = br.readLine();
+            while (line != null) {
+                String[] splitted = line.split(" ");
+                numbers.put(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[0]));
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     NumberTest oddTester;
     NumberTest primeTester;
@@ -11,10 +36,10 @@ public class NumberTester {
     }
 
     public void setPrimeTester(NumberTest primeTester){
-
+    this.primeTester = primeTester;
     }
     public void setPalindromeTester(NumberTest palindromeTester){
-
+    
     }
     public void testFile(){
 
