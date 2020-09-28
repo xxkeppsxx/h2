8fbc429e58946dfae2b5b89081e37b7d78077eea
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-09-28 15:44:16 +0200

    Operator class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         NumberTester tester = new NumberTester("data");
+
         tester.setOddevenTester((number) -> (number %2) ==0);
+
         tester.setPrimeTester(number ->{
             if (number <= 1) {
                 return false;
@@ -13,5 +15,24 @@ public class Main {
             }
             return true;
         });
+
+        tester.setPalindromeTester((number) -> {
+            int palindrome = number; // copied number into variable
+            int reverse = 0;
+
+            while (palindrome != 0) {
+                int remainder = palindrome % 10;
+                reverse = reverse * 10 + remainder;
+                palindrome = palindrome / 10;
+            }
+
+            if (number == reverse) {
+                return true;
+            }
+            return false;
+
+
+        });
+        tester.testFile();
     }
 }
--- src/main/java/NumberTester.java
+++ src/main/java/NumberTester.java
@@ -9,13 +9,13 @@ import java.util.logging.Logger;
 
 public class NumberTester {
     int numberOfTests;
+    Map<Integer, Integer> numbers = new HashMap<>();
+
 
     public NumberTester(String fileName) {
-        Map<Integer,Integer> numbers = new HashMap<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
             String line = br.readLine();
-            numberOfTests = Integer.parseInt(line);
             line = br.readLine();
             while (line != null) {
                 String[] splitted = line.split(" ");
@@ -27,22 +27,46 @@ public class NumberTester {
             Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     NumberTest oddTester;
     NumberTest primeTester;
     NumberTest palindromeTester;
 
-    public void setOddevenTester(NumberTest oddTester){
-    this.oddTester = oddTester;
+    public void setOddevenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
     }
 
-    public void setPrimeTester(NumberTest primeTester){
-    this.primeTester = primeTester;
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
     }
-    public void setPalindromeTester(NumberTest palindromeTester){
 
-        
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
     }
-    public void testFile(){
 
+    public void testFile() {
+        for (int i : numbers.keySet()) {
+            if (i == 1) {
+                if (oddTester.testNumber(numbers.get(i))) {
+                    System.out.println("EVEN");
+                } else {
+                    System.out.println("UNEVEN");
+                }
+            } else if (i == 2) {
+                if (primeTester.testNumber(numbers.get(i))) {
+                    System.out.println("PRIME");
+                } else {
+                    System.out.println("NOTPRIME");
+                }
+            } else if (i == 3) {
+                if(palindromeTester.testNumber(numbers.get(i))) {
+                    System.out.println("PALINDROME");
+                }else{
+                    System.out.println("NON PALINDROME");
+                }
+            }else{
+                System.out.println("ERROR");
+            }
+        }
     }
 }
