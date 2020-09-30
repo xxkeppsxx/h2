5dd9cb194e36a189f7cb493d01874dc1bd4ee61e
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-09-30 18:50:58 +0200

    RationalCalculator created

--- src/main/java/AbstractCalculator.java
+++ src/main/java/AbstractCalculator.java
@@ -1,2 +1,18 @@
 public abstract class AbstractCalculator {
+    protected CalculationOperation add;
+    protected CalculationOperation subtract;
+    protected CalculationOperation multiply;
+    protected CalculationOperation divide;
+
+    public AbstractCalculator(CalculationOperation add,CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide){
+        this.add=add;
+        this.divide=divide;
+        this.multiply = multiply;
+        this.subtract = subtract;
+    }
+    public abstract Number add(Number a, Number b);
+    public abstract Number subtract(Number a, Number b);
+    public abstract Number multiply(Number a, Number b);
+    public abstract Number divide(Number a,Number b);
+
 }
