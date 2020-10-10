5003ede0f5a4ef1433e2d8ce7f4906d103a6d8b6
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-10-10 11:38:34 +0200

    Rationalcalc done

--- src/main/java/CalcMain.java
+++ src/main/java/CalcMain.java
@@ -1,5 +1,29 @@
 public class CalcMain {
     public static void main(String[] args) {
-        
+        RationalCalculator rc = new RationalCalculator((Number x, Number y) -> {
+            Number r = new Number(x.getA() * y.getB(), x.getB() * y.getB());
+            Number r2 = new Number(y.getA() * x.getB(), x.getB() * y.getB());
+            return new Number(r.getA() + r2.getA(), x.getB() * y.getB());
+        },(Number x,Number y) -> {
+            Number r = new Number(x.getA() * y.getB(), x.getB() * y.getB());
+            Number r2 = new Number(y.getA() * x.getB(), x.getB() * y.getB());
+            return new Number(r.getA() - r2.getA(), x.getB() * y.getB());
+        },(Number x,Number y)->{
+            return new Number(x.getA() * y.getA(), x.getB() * y.getB());
+        },(Number x,Number y) ->{
+            return new Number(x.getA()/y.getB(),x.getB()/y.getA());
+        });
+
+        VectorCalculator vc = new VectorCalculator((Number x,Number y) -> {
+            return new Number(x.getA() + y.getA(), x.getB() + y.getB());
+        },(Number x,Number y)-> {
+            return new Number(x.getA() - y.getA(), x.getB() - y.getB());
+
+        },(Number x,Number y) -> {
+            return new Number((x.getA() *y.getB()) - (x.getB()* y.getB()),0);
+        },
+
+        });
+
     }
 }
